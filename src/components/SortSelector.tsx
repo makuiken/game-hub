65499ab3ee1e09efdd8be0d29ce9a775ad1404fa
Micah Kuiken <mkuiken18@outlook.com>
2024-03-17 18:47:16 -0400

    Add react-router-dom and zustand dependencies, update NavBar and SearchInput components, create a store for game query, update useGames hook, and update GenreList and PlatformSelector components

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,12 +1,8 @@
 import { Menu, MenuButton, Button, MenuList, MenuItem } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
+import useGameQueryStore from '../store';
 
-interface Props {
-  onSelectSortOrder: (sortOrder: string) => void;
-  sortOrder: string;
-}
-
-const SortSelector = ({onSelectSortOrder, sortOrder}: Props) => {
+const SortSelector = () => {
   const sortOrders = [
     { value: '', label: 'Relevance' },
     { value: '-added', label: 'Date Added' },
@@ -16,7 +12,11 @@ const SortSelector = ({onSelectSortOrder, sortOrder}: Props) => {
     { value: '-rating', label: 'Average Rating' },
   ];
 
-  const currentSortOrder = sortOrders.find(order => order.value === sortOrder)
+  const setSortOrder = useGameQueryStore((s) => s.setSortOrder);
+  const sortOrder = useGameQueryStore((s) => s.gameQuery.sortOrder);
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === sortOrder
+  );
 
   return (
     <Menu>
@@ -25,7 +25,11 @@ const SortSelector = ({onSelectSortOrder, sortOrder}: Props) => {
       </MenuButton>
       <MenuList>
         {sortOrders.map((order) => (
-          <MenuItem onClick={() => onSelectSortOrder(order.value)} key={order.value} value={order.value}>
+          <MenuItem
+            onClick={() => setSortOrder(order.value)}
+            key={order.value}
+            value={order.value}
+          >
             {order.label}
           </MenuItem>
         ))}
